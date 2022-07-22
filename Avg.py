7d4a7bdf5cd8dc0923471755b6096b06e3113e2e
Devashish204 <109453645+Devashish204@users.noreply.github.com>
2022-07-22 15:36:40 +0530

    Add files via upload

---
+++ Avg.py
@@ -0,0 +1,8 @@
+#Write a Python Program to find the average of two numbers entered by the user
+
+a = input("Enter The First Number: ")
+b = input("Enter The Secound Number: ")
+
+avg = (a+b)/2
+
+print("The Avg of the two number is: ", avg)
