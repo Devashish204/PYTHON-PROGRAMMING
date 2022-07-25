7160d2d0a223f9d107919960e4b432184d37d1ce
Devashish204 <109453645+Devashish204@users.noreply.github.com>
2022-07-25 11:41:58 +0530

    Add files via upload

---
+++ String_functions.py
@@ -0,0 +1,27 @@
+#1 Length Function: This Function returns the lenght of the string.
+
+Story=("Once upon a time there was a youtuber named RBJ he uploaded Gaming videos on his youtube Channel")
+
+print  (len(Story))
+
+#2 EndsWith Function: This Function tells wheather this string is end with given string or not. 
+
+print(Story.endswith("Channel"))
+
+#3 Count Function: This Functions Count the Total Number of Accurance of any Character.
+
+print(Story.count("y"))
+
+#4 Capitalize Function: This Function Capitalize the first character of the string.
+
+Story=("once upon a time there was a youtuber named RBJ he uploaded Gaming videos on his youtube Channel")
+
+print(Story.capitalize())
+
+#5 Find(word) Function: This Function finds a word and returns the index of the first occurane of the word in the string.
+
+print(Story.find("RBJ"))
+
+#6 Replaced(oldword , newword) Function: This Functions replaces the old word with the new word in the entire string
+
+print(Story.replace("RBJ","Dev"))
