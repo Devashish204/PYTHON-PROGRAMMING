3fdf00faef57eb502947c58e1b7e4a46b3724b99
Devashish204 <109453645+Devashish204@users.noreply.github.com>
2022-07-25 16:15:39 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,41 @@
+#L1 = ["Dev" , True , 1  ]
+
+#print(L1)
+
+#List Methods 
+
+# 1.Sort 
+
+L1 = [1,8,7,2,21,15]
+
+L1.sort()
+print(L1)
+
+# 2.Reverse
+
+L2 = [1,4,7,9,0,42,22]
+
+L2.reverse()
+print(L2)
+
+# 3.Append
+
+L3 = [2,65,34,22,4]
+
+L3.append(88)
+print(L3)
+
+# 4.Insert
+
+L3.insert(2,5)
+print(L3)
+
+# 5.POP
+
+L3.pop(2)
+print(L3)
+
+# 5.Remove
+
+L3.remove(88)
+print(L3)
