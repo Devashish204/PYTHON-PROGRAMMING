75f101334f3ce9b8c0d862d88d2183d5114d5bf4
Devashish204 <109453645+Devashish204@users.noreply.github.com>
2022-08-02 01:08:18 +0530

    Add files via upload

---
+++ Rang_Function.py
@@ -0,0 +1,11 @@
+#Range function in Python Programming Language.
+#start and stop rang function.
+for i in range(1,9): #it will print number starting from 1 and will end on 8.
+    print(i)
+
+
+    #step_size rang function.
+
+    # for i in range (1,9,2) #it will skill the value '2' after the 1 it will direct print 3.
+    # print(i) 
+
